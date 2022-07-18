cbc63d26d9f1c8fd130d2d90475ba3781f4c6885
gglyptodon <janina.mass@gmail.com>
2022-07-18 10:13:11 +0200

    add python version

---
+++ scripts/read_loop.py
@@ -0,0 +1,12 @@
+import datetime
+import sys
+def main():
+    try:
+       for line in iter(sys.stdin.readline, b''):
+          print("read @ {}:\n{}".format(datetime.datetime.now(), line))
+    except KeyboardInterrupt:
+       sys.stdout.flush()
+       pass
+
+if __name__=="__main__":
+    main()
